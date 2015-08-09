0bef5b4fde41625e95a7307fadb31c92a6c1b082
Alexandre Giuseppe de Fuccio Pereira <alexandregiuseppe@gmail.com>
2015-08-09 12:25:53 -0300

    Improvements on TerminalDispatcher code organization

--- Trader/TerminalDispatcher.fs
+++ Trader/TerminalDispatcher.fs
@@ -2,7 +2,7 @@
 
 open System
 
-type Color = 
+type private Color = 
     | Black = 30
     | Red = 31
     | Green = 32
@@ -12,28 +12,23 @@ type Color =
     | Cyan = 36
     | White = 37
 
-type Intensity = 
+type private Intensity = 
     | Normal = 0
     | Bright = 2
 
 
-type OrderOwner =
-    | Me
-    | ThirdPart
-    | Group
 
-let IsLinux = System.Type.GetType("Mono.Runtime") <> null
+let private IsLinux = System.Type.GetType("Mono.Runtime") <> null
 
-let ResetCode = 
+let private ResetCode = 
     if IsLinux then "\x1B[m"
     else String.Empty
 
-let GetColorSequence color level =
-    if IsLinux then sprintf "\x1B[%d;%dm" level color
+let private GetColorSequence color level =
+    if IsLinux then sprintf "\x1B[%d;%dm" (int level) (int color)
     else String.Empty
 
-
-let GetColoredValue (value : decimal) color =
+let private GetColoredValue (value : decimal) color =
     let c = GetColorSequence color
     let arr =
         (sprintf "%14.8f" value).ToCharArray()
@@ -41,47 +36,48 @@ let GetColoredValue (value : decimal) color =
         |> Array.fold (fun (tail, valueStarted) x -> 
            let s, v = 
                if (x >= '1' && x <= '9') || valueStarted then 
-                    sprintf "%s%c" (c 0) x, true
+                    sprintf "%s%c" (c Intensity.Normal) x, true
                elif x = '.' then
-                    sprintf "%s%c" (c 2) x, true
+                    sprintf "%s%c" (c Intensity.Bright) x, true
                else 
-                    sprintf "%s%c" (c 2) x, valueStarted
+                    sprintf "%s%c" (c Intensity.Bright) x, valueStarted
            s :: tail, v) ([], false)
 
     String.Concat(Array.ofList (fst arr))
 
+let private PrintTransaction color sign price amount tag =
+    let p1 = (GetColoredValue price color)
+    let p2 = (GetColoredValue amount color)
+    let prefix = (GetColorSequence color Intensity.Normal) + sign
+    let suffix = (GetColorSequence color Intensity.Normal) + tag
+    printfn "%s%s%s [%s]%s" prefix p1 p2 suffix ResetCode
+
 let PrintBalanceData item value =
-    printfn " %-13s:%s%s" item (GetColoredValue value (int Color.White)) ResetCode
+    printfn " %-13s:%s%s" item (GetColoredValue value Color.White) ResetCode
 
 let PrintInfo info =
     let timestamp = DateTime.Now.ToString("HH:mm:ss")
-    printfn "%s[%s] %s%s"(GetColorSequence (int Color.Cyan) 0) timestamp info ResetCode
+    printfn "%s[%s] %s%s"(GetColorSequence Color.Cyan Intensity.Normal) timestamp info ResetCode
     
-let PrintOrderList (price: decimal)  (amount: decimal)  owner =
+type OrderOwner =
+    | Me
+    | ThirdPart
+    | Group    
+    
+let PrintOrderList (price: decimal)  (amount: decimal) owner =
     let color = 
         match owner with
-        | Me -> int Color.Green
-        | Group -> int Color.Yellow
-        | _ -> int Color.White
+        | Me -> Color.Green
+        | Group -> Color.Yellow
+        | _ -> Color.White
 
     let p1 = (GetColoredValue price color)
     let p2 = (GetColoredValue amount color)
     printfn " %s%s%s"  p1 p2 ResetCode
-    
-
-let PrintTransaction color sign price amount tag =
-    let p1 = (GetColoredValue price color)
-    let p2 = (GetColoredValue amount color)
-    let prefix = (GetColorSequence color 0) + sign
-    let suffix = (GetColorSequence color 0) + tag
-    printfn "%s%s%s [%s]%s" prefix p1 p2 suffix ResetCode
-
    
 let PrintCreateOrder (price: decimal)  (amount: decimal) tag =
-    let color = int Color.Blue
-    PrintTransaction color "+" price amount tag
+    PrintTransaction Color.Blue "+" price amount tag
 
 let PrintCancelOrder (price: decimal)  (amount: decimal) tag =
-    let color = int Color.Red
-    PrintTransaction color "-" price amount tag
+    PrintTransaction Color.Red "-" price amount tag
 
